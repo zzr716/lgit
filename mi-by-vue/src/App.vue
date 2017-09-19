67e4fe0059b056cf8e1bef591dfa408cfc4c9ea2
zzr716 <z71634@163.com>
2017-09-19 13:55:38 +0800

    test

---
+++ mi-by-vue/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <top-bar></top-bar>
+    <top-header></top-header>
+    <top-banner></top-banner>
+    <top-sub></top-sub>
+    <goods></goods>
+    <temp-footer></temp-footer>
+    <play-video :play-config="playConfig"></play-video>
+  </div>
+</template>
+
+<script>
+import TopBar from './components/topbar'
+import TopHeader from './components/topheader'
+import TopBanner from './components/topbanner'
+import TopSub from './components/topsub'
+import Goods from './components/goods'
+import TempFooter from './components/tempfooter'
+import PlayVideo from './components/common/playvideo'
+export default {
+  name: 'app',
+  data () {
+    return {
+      playConfig: {}
+    }
+  },
+  ready () {
+    this.recivePlayVideo()
+  },
+  methods: {
+    recivePlayVideo () {
+      this.$on('play', function (opts) {
+        this.playConfig = opts
+      })
+    }
+  },
+  components: {
+    'top-bar': TopBar,
+    'top-header': TopHeader,
+    'top-banner': TopBanner,
+    'top-sub': TopSub,
+    'goods': Goods,
+    'temp-footer': TempFooter,
+    'play-video': PlayVideo
+  }
+}
+</script>
+
+<style>
+@import './assets/css/base.css';
+</style>
---
+++ mi-by-vue/src/components/common/GoodsHeader.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="header-wrap">
+		<h2 class="title">{{title}}</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+		}
+	},
+	props: {
+		title: {
+			required: true,
+			type: String
+		}
+	}
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+.header-wrap {
+	position: relative;
+	.title {
+		margin: 0;
+		font-size: 22px;
+		font-weight: 200;
+		line-height: 58px;
+		color: #333;
+	}
+}
+</style>
